add0c505bdedde8703a4df35c2757f20403d614c
Hieu Vo <hughvo39@gmail.com>
2022-11-19 20:52:53 -0800

    Revert "Refactor"
    This reverts commit 754214158ba1b329945232dddc1223dd28fbcd69.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     // @ts-ignore
     import Navbar from "$lib/NavBar/NavBar.svelte"
-    import MainView from "$lib/MainView.svelte"
+    import MainView from "$lib/MidView.svelte"
 
 </script>
 <main>
